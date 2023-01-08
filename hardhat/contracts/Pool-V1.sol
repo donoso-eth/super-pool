5a9eafbb7c40032d1f4dfb408a0fe52fee9b8fb6
Javier Donoso <javier.donoso@nuilea.com>
2023-01-08 19:02:13 +0100

    merge

--- hardhat/contracts/Pool-V1.sol
+++ hardhat/contracts/Pool-V1.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import "@openzeppelin/contracts/token/ERC777/IERC777.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC1820Registry.sol";
 import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
-import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {UUPSProxiable} from "./upgradability/UUPSProxiable.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
